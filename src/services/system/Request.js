9d0f0d117b589e0c8c3e8fa27c760479b0257568
Maxim Yaroshchuk <maxim.yaroshuk@splynx.com>
2025-01-19 21:27:33 +0200

    Add env

--- src/services/system/Request.js
+++ src/services/system/Request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import NProgress from 'nprogress';
 
-const API_URL = 'http://localhost:3000/';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const apiClient = axios.create({
     baseURL: API_URL,
