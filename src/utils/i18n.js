6cb2f0fff95f6d7bc31b8c434bb844d9971d0f66
Maxim Yaroshchuk <maxim.yaroshuk@splynx.com>
2025-01-31 16:50:20 +0200

    LightConvertWeb remove debug transfer local to public

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,35 +1,26 @@
 import {createI18n} from 'vue-i18n';
-import en from '../locales/en.json';
-import uk from '../locales/uk.json';
-import zh from '../locales/zh.json';
-import ja from '../locales/ja.json';
-import pt from '../locales/pt.json';
-import es from '../locales/es.json';
-import de from '../locales/de.json';
-import ru from '../locales/ru.json';
-import pl from '../locales/pl.json';
+import en from '@/locales/en.json';
+import uk from '@/locales/uk.json';
+import zh from '@/locales/zh.json';
+import ja from '@/locales/ja.json';
+import pt from '@/locales/pt.json';
+import es from '@/locales/es.json';
+import de from '@/locales/de.json';
+import ru from '@/locales/ru.json';
+import pl from '@/locales/pl.json';
 
 // Все доступные переводы
 const messages = {
-    en: processLocaleFile(en),
-    uk: processLocaleFile(uk),
-    zh: processLocaleFile(zh),
-    ja: processLocaleFile(ja),
-    pt: processLocaleFile(pt),
-    es: processLocaleFile(es),
-    de: processLocaleFile(de),
-    ru: processLocaleFile(ru),
-    pl: processLocaleFile(pl)
+    en,
+    uk,
+    zh,
+    ja,
+    pt,
+    es,
+    de,
+    ru,
+    pl
 };
-
-function processLocaleFile(localeData) {
-    try {
-        return JSON.parse(JSON.stringify(localeData));
-    } catch (error) {
-        console.error('Locale parsing error:', error);
-        return {};
-    }
-}
 
 const supportedLanguages = Object.keys(messages);
 
